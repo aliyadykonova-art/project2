e7e2f35f0c740056e6e373864c75ea5615a8cac7
aliyadykonova-art <aliyadykonova@gmail.com>
2026-09-25 10:33:54 +0300

    Delete example.py

--- example.py
+++
@@ -1,9 +0,0 @@
-import os
-from typing import Union
-
-import requests
-
-
-def add(a: Union[int, float], b: Union[int, float]) -> Union[int, float]:
-    """короче складывает числа"""
-    return a + b
